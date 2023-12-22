c8e12b36b4d93a40a66486e52ded299f0835c283
zhaoyingchao <randeng.zhao@tuya.com>
2023-12-22 11:46:53 +0800

    fix: build.sh

--- build.sh
+++ build.sh
@@ -27,7 +27,12 @@ mv ${DEST_PKG_DIR}/addon-services/ELASTICSEARCH/${DEFAULT_VERSION} ${DEST_PKG_DI
 mv ${DEST_PKG_DIR}/common-services/ELASTICSEARCH/${DEFAULT_VERSION} ${DEST_PKG_DIR}/common-services/ELASTICSEARCH/${SERVICE_VERSION}
 mv ${DEST_PKG_DIR}/addon-services/KIBANA/${DEFAULT_VERSION} ${DEST_PKG_DIR}/addon-services/KIBANA/${SERVICE_VERSION}
 mv ${DEST_PKG_DIR}/common-services/KIBANA/${DEFAULT_VERSION} ${DEST_PKG_DIR}/common-services/KIBANA/${SERVICE_VERSION}
-sed -i "" "s/${DEFAULT_VERSION}/${SERVICE_VERSION}/g" `grep ${DEFAULT_VERSION} -rl ${DEST_PKG_DIR}/*`
+# mac需要添加空格
+#sed -i ‘’ "s/${DEFAULT_VERSION}/${SERVICE_VERSION}/g" `grep ${DEFAULT_VERSION} -rl ${DEST_PKG_DIR}/*`
+#sed -i "s/${DEFAULT_VERSION}/${SERVICE_VERSION}/g" `grep ${DEFAULT_VERSION} -rl ${DEST_PKG_DIR}/*`
+#find ${DEST_PKG_DIR} -type f -exec sed -i 's/${DEFAULT_VERSION}/${SERVICE_VERSION}/g' {} \;
+find ${DEST_PKG_DIR} -type f -exec perl -pi -e "s/${DEFAULT_VERSION}/${SERVICE_VERSION}/g" {} \;
+
 cd ${BUILD_DIR} && tar zcf ${BUILD_DIR}/${SRC_PKG_NAME}.tar.gz ${SRC_PKG_NAME}
 rm -rf ${DIST_DIR} && mkdir -p ${DIST_DIR} && mv ${BUILD_DIR}/${SRC_PKG_NAME}.tar.gz ${DIST_DIR}
 rm -rf ${BUILD_DIR}
